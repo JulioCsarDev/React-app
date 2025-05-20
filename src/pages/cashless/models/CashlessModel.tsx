8c7dd6156ec33ba1c12841094c9d753ebf17f3a2
Jorge Gomez <jorgeandresg1207@gmail.com>
2025-05-20 09:36:07 -0500

    add cashless

---
+++ src/pages/cashless/models/CashlessModel.tsx
@@ -0,0 +1,9 @@
+export interface CashlessModel {
+    codigo_cliente: Number;
+    cliente_nombre: String;
+    Documento_pedido: Number;
+    placa_vehiculo: String;
+    numero_cliente: Number;
+    novedad: Boolean;
+    obvservacion: String;
+}
