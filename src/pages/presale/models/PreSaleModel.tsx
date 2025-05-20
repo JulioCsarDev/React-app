0bcdbcaad338a1ca441b7a4860885d242e2ab7eb
Jorge Gomez <jorgeandresg1207@gmail.com>
2025-05-20 09:54:18 -0500

    add Pre-Sale

---
+++ src/pages/presale/models/PreSaleModel.tsx
@@ -0,0 +1,12 @@
+export interface PreSaleModel {
+    codigo_cliente: Number;
+    cliente_nombre: String;
+    Municipio: String;
+    codigo_zona: String;
+    codixgo_grupo: String;
+    blacklist: Boolean;
+    suma_cajas: Number;
+    contacto: Number;   
+    novedad: Boolean;
+    obvservacion: String;
+}
